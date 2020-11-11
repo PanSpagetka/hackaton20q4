baa0b3f17ec13e849100305202a0dfc3aa4f2a57
Patrik Komanek <pkomanek@redhat.com>
2020-11-11 15:44:18 +0100

    adding pet model and controller.

--- api/pet_api.rb
+++ api/pet_api.rb
@@ -1,5 +1,133 @@
 require 'json'
 
+class PetController
+  class << self
+    attr_accessor :current_id
+
+    def add_new(params)
+      new = load_pets
+      new.append(
+        Pet.new(
+          self.current_id, 
+          params['name'], 
+          params['description'], 
+          params['status'], 
+          params['tags']
+        )
+      )
+      save_pets(new.to_json)
+      self.current_id += 1
+    end
+
+    def find_by_id(id)
+      load_pets.select { |pet| pet.id == id }
+    end
+
+    #Returns all pets having status included in status array
+    def find_by_status(status)
+      load_pets.select { |pet| status.include?(pet.status) }
+    end
+
+    #Returns all pets containing all of the tags
+    def find_by_tags(tags)
+      load_pets.select do |pet|
+        tags.all? do |tag|
+          pet.tagged_with?(tag)
+        end
+      end
+    end
+
+    def find_by_tag(tag)
+      load_pets.select { |pet| pet.tags.include?(tag) }
+    end
+
+    def delete_pet(id)
+      size = load_pets.size
+      save_pets(load_pets.select { |o| o.id != id }.to_json)
+      return size - load_pets.size
+    end
+
+    private
+
+    def load_pets
+        f =  File.read("pets.json")
+
+      res = Array(JSON.parse(f)).map do |record|
+        Pet.new(
+          record['id'].to_i, 
+          record['name'], 
+          record['description'], 
+          record['status'], 
+          record['tags']) 
+      end
+      self.current_id = res.last.id.to_i + 1 || 1
+
+      res
+    rescue => err
+      puts err.message
+      raise
+    end
+
+    def save_pets(json)
+      File.open("pets.json","w") do |file|
+        JSON.pretty_generate(JSON.parse(json)).lines do |line|
+          file.write(line)
+        end
+      end
+    rescue => err
+      puts err.message
+      raise
+    end
+  end
+end
+
+class Pet
+  attr_accessor :name, :description, :status, :tags
+  attr_reader :id
+
+  def initialize(id, name, description, status, tags)
+    self.id          = id
+    self.name        = name
+    self.description = description
+    self.status      = status || 'unknown'
+    self.tags        = tags || []
+  end
+
+  def tagged_with?(tag)
+    tags.include?(tag)
+  end
+
+  def tag_with(tag)
+    self.tags << tag
+  end
+
+  def untag(tag)
+    tags.delete(tag)
+  end
+
+  def ==(other)
+    self.id          == other.id &&
+    self.name        == other.name &&
+    self.description == other.description &&
+    self.status      == other.status &&
+    self.tags        == other.tags
+  end
+
+  def to_json(*a)
+    {
+      "id"          => id,
+      "name"        => name,
+      "description" => description,
+      "status"      => status,
+      "tags"        => tags
+  }.to_json(*a)
+  end
+
+  private
+
+  attr_writer :id
+end
+
 
 MyApp.add_route('POST', '/v2/pet', {
   "resourcePath" => "/Pet",
@@ -19,12 +147,7 @@ MyApp.add_route('POST', '/v2/pet', {
   cross_origin
   # the guts live here
 
-  f =  File.read("pets.json")
-  pets = Array(JSON.parse(f))
-
-  File.open("pets.json","w") do |f|
-    f.write(JSON.pretty_generate(pets.append(params)))
-  end
+  PetController.add_new(params)
 
   {"message" => "New pet, yay!"}.to_json
 end
@@ -54,7 +177,7 @@ MyApp.add_route('DELETE', '/v2/pet/{petId}', {
   cross_origin
   # the guts live here
 
-  {"message" => "yes, it worked"}.to_json
+  {"message" => "Yes, it worked. #{PetController.delete_pet(params["petId"].to_i)} records deleted."}.to_json
 end
 
 
@@ -77,14 +200,14 @@ MyApp.add_route('GET', '/v2/pet/findByStatus', {
   cross_origin
   # the guts live here
 
-  {"message" => "yes, it worked"}.to_json
+  {"message" => PetController.find_by_status(params)}.to_json
 end
 
 
 MyApp.add_route('GET', '/v2/pet/findByTags', {
   "resourcePath" => "/Pet",
   "summary" => "Finds Pets by tags",
-  "nickname" => "find_pets_by_tags", 
+  "nickname" => "find_by_tags", 
   "responseClass" => "Array<Pet>",
   "endpoint" => "/pet/findByTags", 
   "notes" => "Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.",
@@ -100,14 +223,14 @@ MyApp.add_route('GET', '/v2/pet/findByTags', {
   cross_origin
   # the guts live here
 
-  {"message" => "yes, it worked"}.to_json
+  {"message" => PetController.find_by_tags(params.keys)}.to_json
 end
 
 
 MyApp.add_route('GET', '/v2/pet/{petId}', {
   "resourcePath" => "/Pet",
   "summary" => "Find pet by ID",
-  "nickname" => "get_pet_by_id", 
+  "nickname" => "find_by_id", 
   "responseClass" => "Pet",
   "endpoint" => "/pet/{petId}", 
   "notes" => "Returns a single pet",
@@ -122,7 +245,7 @@ MyApp.add_route('GET', '/v2/pet/{petId}', {
   cross_origin
   # the guts live here
 
-  {"message" => "yes, it worked"}.to_json
+  {"message" => PetController.find_by_id(params['petId'].to_i)}.to_json
 end
 
 
@@ -144,7 +267,7 @@ MyApp.add_route('PUT', '/v2/pet', {
   cross_origin
   # the guts live here
 
-  {"message" => "yes, it worked"}.to_json
+  {"message" => "NotImplementedYet"}.to_json
 end
 
 
@@ -166,7 +289,7 @@ MyApp.add_route('POST', '/v2/pet/{petId}', {
   cross_origin
   # the guts live here
 
-  {"message" => "yes, it worked"}.to_json
+  {"message" => "NotImplementedYet"}.to_json
 end
 
 
@@ -188,6 +311,5 @@ MyApp.add_route('POST', '/v2/pet/{petId}/uploadImage', {
   cross_origin
   # the guts live here
 
-  {"message" => "yes, it worked"}.to_json
+  {"message" => "NotImplementedYet"}.to_json
 end
-
